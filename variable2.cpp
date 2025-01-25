da1139462d54d3c85e8d092286cda5df5a10f9b8
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-25 10:32:52 +0530

    new

---
+++ variable2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int glo = 6;
+
+void sum(){
+    int a;
+    cout<<glo;
+}
+
+int main(){
+    // int a = 4;
+    // int b = 5;
+    
+    int glo = 7;
+    int a=4 ,b=5;
+    float pi = 3.15;
+    char n = 'n';
+    char name[] = "koushik";
+    string newC ="hi"; 
+    cout << glo;
+    sum();
+    cout << pi <<n<<name<<newC;
+    cout<<"this is tutorial 4"<<a<<b;
+    return 0;
+}
