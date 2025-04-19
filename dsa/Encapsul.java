45f2bc2a54ca20586362d6da1b647eebbeb5fe60
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-04-19 10:27:48 +0530

    new

---
+++ dsa/Encapsul.java
@@ -0,0 +1,12 @@
+
+
+
+import bank.*;
+
+public class Encapsul {
+    public static void main(String args[]){
+        Bank ac = new Bank();
+        ac.name ="k";
+        ac.here();
+    }
+}
---
+++ dsa/bank/Bank.java
@@ -0,0 +1,10 @@
+package bank;
+class Account {
+
+}
+public class Bank {
+    public String name;
+    public void here(){
+        System.out.println(this.name);
+    }
+}
