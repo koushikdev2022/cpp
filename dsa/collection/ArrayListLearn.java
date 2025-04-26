de795687dfa7f62b8b44cb365751928ea6c33a63
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-04-26 15:47:38 +0530

    new

--- dsa/collection/ArrayListLearn.java
+++ dsa/collection/ArrayListLearn.java
@@ -1,12 +1,15 @@
 // package collection;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
 import java.util.Stack;
-
+import java.util.TreeSet;
 public class ArrayListLearn {
     public static void main(String args[]){
         ArrayList<String> studentName = new ArrayList<>();
@@ -109,5 +112,44 @@ public class ArrayListLearn {
         adq.pollFirst();
         System.out.println(adq);
 
+
+        //has no order can obtains randoms
+        //only can set same element means if there is one 54 second 54 can not assign
+        Set<Integer> set = new HashSet<>();
+        set.add(12);
+        set.add(13);
+        set.add(13);
+        set.add(14);
+        set.remove(12);
+
+        System.out.println(set);
+        System.out.println(set.contains(13));
+        set.clear();
+
+        //same but it is linklist set insert into set 1 by one
+        Set<Integer> setl = new LinkedHashSet<>();
+        setl.add(12);
+        setl.add(13);
+        setl.add(13);
+        setl.add(14);
+        setl.remove(12);
+
+        System.out.println(setl);
+        System.out.println(setl.contains(13));
+        setl.clear();
+
+
+        // graph tree for shorted format
+        
+        Set<Integer> setlt = new TreeSet<>();
+        setlt.add(12);
+        setlt.add(13);
+        setlt.add(13);
+        setlt.add(14);
+        setlt.remove(12);
+
+        System.out.println(setlt);
+        System.out.println(setlt.contains(13));
+        setlt.clear();
     }
 }
